c1ed48ed3cadc59420eb3232fbcf614938647643
aimbot1526 <anshjaamwal2002@gmail.com>
2022-01-25 03:50:34 +0530

    files upload done!

--- src/main/java/com/org/dumper/service/PropertyService.java
+++ src/main/java/com/org/dumper/service/PropertyService.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -45,8 +46,13 @@ public class PropertyService {
 
             PropertyImages images = new PropertyImages();
 
+            String destination = "F:/images/" + file.getOriginalFilename();
+
+            File fileP = new File(destination);
+
             try {
                 images.setData(file.getBytes());
+                file.transferTo(fileP);
             } catch (IOException e) {
                 e.printStackTrace();
             }
