6756693ac9b3e05de69ed006de6d2a533acb0220
aimbot1526 <anshjaamwal2002@gmail.com>
2022-02-07 13:12:41 +0530

    pushes-to-develop

---
+++ src/main/java/com/org/dumper/service/UserService.java
@@ -0,0 +1,93 @@
+package com.org.dumper.service;
+
+import com.org.dumper.dto.FavPropertiesDto;
+import com.org.dumper.dto.UserDto;
+import com.org.dumper.mapper.FavPropertiesMapper;
+import com.org.dumper.mapper.UserMapper;
+import com.org.dumper.model.FavProperties;
+import com.org.dumper.model.Property;
+import com.org.dumper.model.User;
+import com.org.dumper.payload.request.UserProfileRequest;
+import com.org.dumper.repository.FavRepository;
+import com.org.dumper.repository.PropertyRepository;
+import com.org.dumper.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final PropertyRepository propertyRepository;
+
+    private final UserRepository userRepository;
+
+    private final FavRepository favRepository;
+
+    private final UserMapper userMapper;
+
+    private final FavPropertiesMapper favPropertiesMapper;
+
+    public String addFavProperty(Long userId, Long propertyId, boolean isFav) {
+
+        Property property = propertyRepository.findById(propertyId)
+                .orElseThrow(() -> new RuntimeException("Property not found with id: " + propertyId));
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
+
+        Optional<FavProperties> favProperties = favRepository.findByUsersIdAndPropertyId(userId, propertyId);
+
+        if (favProperties.isPresent()) {
+            favProperties.stream().forEach(prop -> {
+                prop.setFavorite(isFav);
+                favRepository.save(prop);
+            });
+        } else {
+            FavProperties properties = new FavProperties();
+
+            properties.setFavorite(isFav);
+            properties.setProperty(property);
+            properties.setUsers(user);
+            favRepository.save(properties);
+        }
+
+        return "User successfully changed value to : " + isFav;
+    }
+
+    public List<FavPropertiesDto> getFavProperties(Long userId) {
+
+        List<FavProperties> propertiesList = favRepository.findAllByUsersId(userId);
+
+        return favPropertiesMapper.map(propertiesList);
+
+    }
+
+    public UserDto getUserProfile(Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with id :" + userId));
+
+        return userMapper.toDto(user);
+    }
+
+    public UserDto updateUserProfile(UserProfileRequest request, Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with id :" + userId));
+
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+
+        userRepository.save(user);
+
+        return userMapper.toDto(user);
+
+    }
+}
